d26092364577a1bc88ab41d75fa7d2499635c8d4
steve oler <stevejonoler@gmail.com>
2019-05-23 11:02:52 -0600

    created game object and capturing number of players and cards

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+class Game:
+    def __init__(self, num_players, num_cards):
+        self.num_players = num_players
+        self.num_cards = num_cards
+
+    def play (self):
+        print("playing game")
+
+
 print("Welcome to the pyramid game!")
 print("How many players are there? (Must be at least 2)")
+num_players = int(input())
+print("How many cards do you want each player to have?")
+num_cards = int(input())
+game = Game(num_players, num_cards)
+game.play()
